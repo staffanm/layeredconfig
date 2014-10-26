49001bd36a00ae29e15cace635a7032be465026a
Staffan Malmgren <staffan.malmgren@gmail.com>
2014-10-26 21:27:38 +0100

    add intersphinx, missing api RST

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ import layeredconfig
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.viewcode', 'sphinx.ext.intersphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -273,3 +273,6 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+intersphinx_mapping = {'python': ('http://docs.python.org/3/', None),
+                       }
