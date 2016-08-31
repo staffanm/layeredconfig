da7b9a445301b15b974ba610231438ec468694d1
Staffan Malmgren <staffan.malmgren@gmail.com>
2016-08-31 20:22:28 +0200

    final release prep

--- layeredconfig/__init__.py
+++ layeredconfig/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Staffan Malmgren'
 __email__ = 'staffan.malmgren@gmail.com'
-__version__ = "0.3.1.dev1"
+__version__ = "0.3.1"
 
 from .layeredconfig import LayeredConfig
 from .configsource import ConfigSource
